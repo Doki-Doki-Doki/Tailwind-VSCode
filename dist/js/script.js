58f35a080c6d35d53217cba8b883f9fb40169ab9
LegendaryPaul <paulanjelozzz@gmail.com>
2023-08-02 15:37:09 +0800

    first commit

---
+++ dist/js/script.js
@@ -0,0 +1,7 @@
+const toggleMenu = document.querySelector('.toggle__menu');
+const headerNav = document.querySelector('.header__drop');
+
+toggleMenu.addEventListener('click',()=>{
+    headerNav.classList.toggle('!h-[20rem]');
+})
+
